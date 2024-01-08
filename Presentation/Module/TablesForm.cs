f810790195bcd79b280d71e334796126645ca5d5
mysCod3r <m.yasinsenocak@gmail.com>
2024-01-08 07:02:45 +0300

    fixed tables form

--- Presentation/Module/TablesForm.cs
+++ Presentation/Module/TablesForm.cs
@@ -2,9 +2,6 @@
 using RayihaRestaurant.Core.Socket;
 using RayihaRestaurant.Data.Service;
 using RayihaRestaurant.Data;
-using System.Runtime.InteropServices;
-using RayihaRestaurant.Core.Enums;
-using RayihaRestaurant.Core.Extensions;
 using RayihaRestaurant.Presentation.Components;
 using RayihaRestaurant.Core.Base;
 
@@ -14,22 +11,18 @@ namespace RayihaRestaurant.Presentation.Module.Views
     public partial class TablesForm : BaseForm
     {
         public override Size WindowSize => new Size(650, 510);
-        bool mouseDown;
+        public override string WindowPanelName => "Masalar";
         private List<Table> _tables;
         private ClientType _clientType;
         private SocketServer _socketServer;
         private readonly CashierService _service;
-        public override string WindowPanelName => "Masalar " + _clientType.ToString();
-
-
-
 
         public TablesForm(ClientType clientType, SocketServer socketServer)
         {
-            _socketServer = socketServer;
             _service = new CashierService(new DatabaseContext());
-            _tables = _service.GetTables();
+            _socketServer = socketServer;
             _clientType = clientType;
+            _tables = _service.GetTables();
             InitializeComponent();
             CreateTablePanels();
         }
