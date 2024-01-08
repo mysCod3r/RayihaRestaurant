8c433772ed070e1a14e02a1eb987af3079dfa95f
mysCod3r <m.yasinsenocak@gmail.com>
2024-01-08 06:45:54 +0300

    completed #26

--- Presentation/Auth/LoginForm.Designer.cs
+++ Presentation/Auth/LoginForm.Designer.cs
@@ -29,149 +29,103 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
-            panel1 = new Panel();
-            label1 = new Label();
-            button1 = new Button();
-            panel2 = new Panel();
-            pictureBox2 = new PictureBox();
-            pictureBox1 = new PictureBox();
-            label4 = new Label();
-            label3 = new Label();
+            pnlContent = new Panel();
+            picPassword = new PictureBox();
+            picUsername = new PictureBox();
+            lblLogin = new Label();
+            lblPassword = new Label();
             txtPassword = new TextBox();
-            label2 = new Label();
+            lblUsername = new Label();
             txtUsername = new TextBox();
             btnLogin = new Button();
-            panel3 = new Panel();
-            panel1.SuspendLayout();
-            panel2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            btnClose = new Button();
+            pnlContent.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)picPassword).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)picUsername).BeginInit();
             SuspendLayout();
             // 
-            // panel1
+            // pnlContent
             // 
-            panel1.BackColor = Color.FromArgb(0, 7, 92);
-            panel1.Controls.Add(label1);
-            panel1.Controls.Add(button1);
-            panel1.Dock = DockStyle.Top;
-            panel1.Location = new Point(0, 0);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(879, 37);
-            panel1.TabIndex = 0;
-            panel1.Paint += panel1_Paint;
-            panel1.MouseDown += panel1_MouseDown;
-            panel1.MouseMove += panel1_MouseMove;
-            panel1.MouseUp += panel1_MouseUp;
+            pnlContent.BackColor = Color.Transparent;
+            pnlContent.Controls.Add(picPassword);
+            pnlContent.Controls.Add(picUsername);
+            pnlContent.Controls.Add(lblLogin);
+            pnlContent.Controls.Add(lblPassword);
+            pnlContent.Controls.Add(txtPassword);
+            pnlContent.Controls.Add(lblUsername);
+            pnlContent.Controls.Add(txtUsername);
+            pnlContent.Controls.Add(btnLogin);
+            pnlContent.Location = new Point(285, 124);
+            pnlContent.Name = "pnlContent";
+            pnlContent.Size = new Size(369, 320);
+            pnlContent.TabIndex = 4;
             // 
-            // label1
+            // picPassword
             // 
-            label1.AutoSize = true;
-            label1.ForeColor = Color.White;
-            label1.Location = new Point(25, 8);
-            label1.Name = "label1";
-            label1.Size = new Size(127, 20);
-            label1.TabIndex = 1;
-            label1.Text = "Rayiha Restaurant";
-            label1.Click += label1_Click;
+            picPassword.Image = (Image)resources.GetObject("picPassword.Image");
+            picPassword.Location = new Point(24, 177);
+            picPassword.Name = "picPassword";
+            picPassword.Size = new Size(33, 27);
+            picPassword.SizeMode = PictureBoxSizeMode.CenterImage;
+            picPassword.TabIndex = 11;
+            picPassword.TabStop = false;
             // 
-            // button1
+            // picUsername
             // 
-            button1.BackgroundImage = (Image)resources.GetObject("button1.BackgroundImage");
-            button1.BackgroundImageLayout = ImageLayout.Center;
-            button1.Dock = DockStyle.Right;
-            button1.FlatAppearance.BorderSize = 0;
-            button1.FlatStyle = FlatStyle.Flat;
-            button1.Location = new Point(832, 0);
-            button1.Name = "button1";
-            button1.Size = new Size(47, 37);
-            button1.TabIndex = 0;
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click_1;
+            picUsername.Image = (Image)resources.GetObject("picUsername.Image");
+            picUsername.Location = new Point(24, 120);
+            picUsername.Name = "picUsername";
+            picUsername.Size = new Size(33, 27);
+            picUsername.SizeMode = PictureBoxSizeMode.CenterImage;
+            picUsername.TabIndex = 10;
+            picUsername.TabStop = false;
             // 
-            // panel2
+            // lblLogin
             // 
-            panel2.BackColor = Color.Transparent;
-            panel2.Controls.Add(pictureBox2);
-            panel2.Controls.Add(pictureBox1);
-            panel2.Controls.Add(label4);
-            panel2.Controls.Add(label3);
-            panel2.Controls.Add(txtPassword);
-            panel2.Controls.Add(label2);
-            panel2.Controls.Add(txtUsername);
-            panel2.Controls.Add(btnLogin);
-            panel2.Location = new Point(285, 124);
-            panel2.Name = "panel2";
-            panel2.Size = new Size(369, 320);
-            panel2.TabIndex = 4;
+            lblLogin.AutoSize = true;
+            lblLogin.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblLogin.ForeColor = Color.White;
+            lblLogin.Location = new Point(161, 26);
+            lblLogin.Name = "lblLogin";
+            lblLogin.Size = new Size(53, 21);
+            lblLogin.TabIndex = 9;
+            lblLogin.Text = "Login";
             // 
-            // pictureBox2
+            // lblPassword
             // 
-            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
-            pictureBox2.Location = new Point(24, 177);
-            pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(33, 27);
-            pictureBox2.SizeMode = PictureBoxSizeMode.CenterImage;
-            pictureBox2.TabIndex = 11;
-            pictureBox2.TabStop = false;
-            // 
-            // pictureBox1
-            // 
-            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(24, 120);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(33, 27);
-            pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
-            pictureBox1.TabIndex = 10;
-            pictureBox1.TabStop = false;
-            // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label4.ForeColor = Color.White;
-            label4.Location = new Point(161, 26);
-            label4.Name = "label4";
-            label4.Size = new Size(64, 28);
-            label4.TabIndex = 9;
-            label4.Text = "Login";
-            label4.Click += label4_Click;
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label3.ForeColor = Color.White;
-            label3.Location = new Point(63, 154);
-            label3.Name = "label3";
-            label3.Size = new Size(76, 20);
-            label3.TabIndex = 8;
-            label3.Text = "Password";
+            lblPassword.AutoSize = true;
+            lblPassword.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblPassword.ForeColor = Color.White;
+            lblPassword.Location = new Point(63, 154);
+            lblPassword.Name = "lblPassword";
+            lblPassword.Size = new Size(59, 15);
+            lblPassword.TabIndex = 8;
+            lblPassword.Text = "Password";
             // 
             // txtPassword
             // 
             txtPassword.Location = new Point(63, 177);
             txtPassword.Name = "txtPassword";
-            txtPassword.Size = new Size(268, 27);
+            txtPassword.Size = new Size(268, 23);
             txtPassword.TabIndex = 7;
             txtPassword.UseSystemPasswordChar = true;
             // 
-            // label2
+            // lblUsername
             // 
-            label2.AutoSize = true;
-            label2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label2.ForeColor = Color.White;
-            label2.Location = new Point(63, 97);
-            label2.Name = "label2";
-            label2.Size = new Size(80, 20);
-            label2.TabIndex = 6;
-            label2.Text = "Username";
-            label2.Click += label2_Click;
+            lblUsername.AutoSize = true;
+            lblUsername.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblUsername.ForeColor = Color.White;
+            lblUsername.Location = new Point(63, 97);
+            lblUsername.Name = "lblUsername";
+            lblUsername.Size = new Size(64, 15);
+            lblUsername.TabIndex = 6;
+            lblUsername.Text = "Username";
             // 
             // txtUsername
             // 
             txtUsername.Location = new Point(63, 120);
             txtUsername.Name = "txtUsername";
-            txtUsername.Size = new Size(268, 27);
+            txtUsername.Size = new Size(268, 23);
             txtUsername.TabIndex = 5;
             // 
             // btnLogin
@@ -188,56 +142,53 @@
             btnLogin.UseVisualStyleBackColor = true;
             btnLogin.Click += btnLogin_Click;
             // 
-            // panel3
+            // btnClose
             // 
-            panel3.BackColor = Color.Transparent;
-            panel3.Dock = DockStyle.Fill;
-            panel3.Location = new Point(0, 37);
-            panel3.Name = "panel3";
-            panel3.Size = new Size(879, 494);
-            panel3.TabIndex = 5;
+            btnClose.FlatAppearance.BorderColor = Color.White;
+            btnClose.FlatAppearance.MouseOverBackColor = Color.FromArgb(87, 73, 76);
+            btnClose.FlatStyle = FlatStyle.Flat;
+            btnClose.ForeColor = Color.White;
+            btnClose.Location = new Point(655, 30);
+            btnClose.Name = "btnClose";
+            btnClose.Size = new Size(187, 50);
+            btnClose.TabIndex = 12;
+            btnClose.Text = "Kapat";
+            btnClose.UseVisualStyleBackColor = true;
+            btnClose.Click += btnClose_Click;
             // 
-            // WaiterForm
+            // LoginForm
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.Black;
             BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
             BackgroundImageLayout = ImageLayout.Stretch;
             ClientSize = new Size(879, 531);
-            Controls.Add(panel2);
-            Controls.Add(panel3);
-            Controls.Add(panel1);
+            Controls.Add(btnClose);
+            Controls.Add(pnlContent);
             Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
             ForeColor = Color.White;
             FormBorderStyle = FormBorderStyle.None;
             Margin = new Padding(3, 4, 3, 4);
-            Name = "WaiterForm";
+            Name = "LoginForm";
             Text = "WaiterForm";
-            FormClosed += WaiterForm_FormClosed;
-            Load += WaiterForm_Load;
-            panel1.ResumeLayout(false);
-            panel1.PerformLayout();
-            panel2.ResumeLayout(false);
-            panel2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            pnlContent.ResumeLayout(false);
+            pnlContent.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)picPassword).EndInit();
+            ((System.ComponentModel.ISupportInitialize)picUsername).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
-        private Panel panel1;
-        private Button button1;
-        private Label label1;
-        private Panel panel2;
-        private Label label3;
+        private Panel pnlContent;
+        private Label lblLogin;
+        private Label lblUsername;
+        private Label lblPassword;
+        private PictureBox picPassword;
+        private PictureBox picUsername;
         private TextBox txtPassword;
-        private Label label2;
         private TextBox txtUsername;
         private Button btnLogin;
-        private Label label4;
-        private PictureBox pictureBox2;
-        private PictureBox pictureBox1;
-        private Panel panel3;
+        private Button btnClose;
     }
 }
--- Presentation/Auth/LoginForm.cs
+++ Presentation/Auth/LoginForm.cs
@@ -1,94 +1,35 @@
 ﻿
+using Rayiha.Presentation.Kitchen;
+using RayihaRestaurant;
 using RayihaRestaurant.Core.Models;
 using RayihaRestaurant.Core.Socket;
 using RayihaRestaurant.Data;
 using RayihaRestaurant.Data.Service;
+using RayihaRestaurant.Presentation.Module.Views;
 
 namespace Rayiha.Presentation.Waiter
 {
     public partial class LoginForm : Form
     {
-        bool mouseDown;
-        private readonly SocketClient _socketClient;
-        private readonly ClientType _clientType;
         private readonly AuthService _service;
-        private User _user;
-        public LoginForm()
+        private SocketServer _socketServer;
+        private MainForm _mainForm;
+        private KitchenForm _kitchenForm = new KitchenForm();
+
+        public LoginForm(SocketServer socketServer, MainForm mainForm)
         {
             _service = new AuthService(new DatabaseContext());
+            _mainForm = mainForm;
+            _socketServer = socketServer;
+            _socketServer.AddMessageHandler(_kitchenForm);
             InitializeComponent();
-            _socketClient = new SocketClient();
-            _clientType = ClientType.Waiter;
-        }
-
-        private void WaiterForm_Load(object sender, EventArgs e)
-        {
-            MessageModel messageModel = new MessageModel { sender = _clientType, message = "form açıldı" };
-            _socketClient.SendMessage(messageModel);
-        }
-
-        private void WaiterForm_FormClosed(object sender, FormClosedEventArgs e)
-        {
-            MessageModel messageModel = new MessageModel { sender = _clientType, message = "form kapandı" };
-            _socketClient.SendMessage(messageModel);
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            MessageModel messageModel = new MessageModel { sender = _clientType, message = "deneme" };
-            _socketClient.SendMessage(messageModel);
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label4_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button1_Click_1(object sender, EventArgs e)
-        {
-            Close();
-        }
-
-        private void panel1_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void panel1_MouseDown(object sender, MouseEventArgs e)
-        {
-            mouseDown = true;
-        }
-
-        private void panel1_MouseMove(object sender, MouseEventArgs e)
-        {
-            if (mouseDown)
-            {
-                int x = MousePosition.X - 400;
-                int y = MousePosition.Y - 20;
-                this.SetDesktopLocation(x, y);
-            }
-        }
-
-        private void panel1_MouseUp(object sender, MouseEventArgs e)
-        {
-            mouseDown = false;
         }
 
         private void btnLogin_Click(object sender, EventArgs e)
         {
             string username = txtUsername.Text;
             string password = txtPassword.Text;
-            _user = _service.AuthenticateUser(username, password);
+            User _user = _service.AuthenticateUser(username, password);
 
             if (_user != null)
             {
@@ -96,13 +37,13 @@ namespace Rayiha.Presentation.Waiter
                 switch (_user.Type)
                 {
                     case UserType.Waiter:
-                        MessageBox.Show(UserType.Waiter.ToString());
+                        new TablesForm(socketServer: _socketServer, clientType: ClientType.Waiter).Show();
                         break;
                     case UserType.Cashier:
-                        MessageBox.Show("Cashier");
+                        new TablesForm(socketServer: _socketServer, clientType: ClientType.Cashier).Show();
                         break;
                     case UserType.Chef:
-                        MessageBox.Show("Kitchen");
+                        _kitchenForm.Open();
                         break;
                     default:
                         MessageBox.Show("Bilinmeyen Kullanıcı Türü");
@@ -115,5 +56,10 @@ namespace Rayiha.Presentation.Waiter
             }
         }
 
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            _mainForm.Show();
+        }
     }
 }
